7beb481814f698d1ece9cea66165fbf59692c36c
gnfrost <frostgalactic@gmail.com>
2025-06-08 21:59:15 +0700

    nothing unessecary

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Multi Creteria Desicion Making</h1>", unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: center;'>Vikor & Promethee</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; '>Choosing the best supplier for Rumah Atsiri</h2>", unsafe_allow_html=True)
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.page_link("pages/vikorGPT.py", label="vikor", icon="📊")
+
+with col2:
+    st.page_link("pages/promethee.py", label="Promethee", icon="📈")
